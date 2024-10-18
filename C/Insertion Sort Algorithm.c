4b42182fcf945afed7a25c33945eb467bb9cc971
sathya2728 <161938442+sathya2728@users.noreply.github.com>
2024-10-18 17:31:53 +0530

    Update Insertion Sort Algorithm.c

--- C/Insertion Sort Algorithm.c
+++ C/Insertion Sort Algorithm.c
@@ -1,42 +1,54 @@
-// C program for insertion sort
-#include <math.h>
+//the code is modified to get input from users
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n)
-{
-	int i, key, j;
-	for (i = 1; i < n; i++) {
-		key = arr[i];
-		j = i - 1;
-		/* Move elements of arr[0..i-1], that are
-		greater than key, to one position ahead
-		of their current position */
-		while (j >= 0 && arr[j] > key) {
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
+/* Function to sort an array using insertion sort */
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        /* Move elements of arr[0..i-1] that are greater than key, to one position ahead of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 // A utility function to print an array of size n
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
 
 /* Driver program to test insertion sort */
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+int main() {
+    int n;
 
-	insertionSort(arr, n);
-	printArray(arr, n);
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
 
-	return 0;
+    // Dynamic memory allocation for the array
+    int *arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1; // Exit if memory allocation fails
+    }
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+    
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    // Free the allocated memory
+    free(arr);
+    return 0;
 }
