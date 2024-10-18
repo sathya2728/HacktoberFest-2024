e65c4d1f006a85f31a9b29b904852d1006b21c6b
sathya2728 <161938442+sathya2728@users.noreply.github.com>
2024-10-18 18:30:04 +0530

    Update dictionary in Python.py
    code modified

--- python/dictionary in Python.py
+++ python/dictionary in Python.py
@@ -1,10 +1,19 @@
-# Create an empty dictionary
+
 my_dict = {}
 
+def add_entry(key, value):
+    my_dict[key] = value
+
+# Function to display the dictionary
+def display_dict():
+    print("Dictionary Contents:")
+    for key, value in my_dict.items():
+        print(f"{key}: {value}")
+
 # Add key-value pairs to the dictionary
-my_dict['name'] = 'John'
-my_dict['age'] = 30
-my_dict['city'] = 'New York'
+add_entry('name', 'John')
+add_entry('age', 30)
+add_entry('city', 'New York')
 
 # Print the dictionary
-print(my_dict)
+display_dict()
