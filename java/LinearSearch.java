f76c084c51fade2a1c29e8a01020ef7d9093fd85
sathya2728 <161938442+sathya2728@users.noreply.github.com>
2024-10-18 17:56:41 +0530

    Update LinearSearch.java

--- java/LinearSearch.java
+++ java/LinearSearch.java
@@ -1,19 +1,41 @@
+//code modified
 import java.util.Scanner;
-class Example1
-{
-   public static void main(String args[])
-   {
-      int i,key=9;
-      int a[]={1,4,7,9,10};     
-      for (i= 0; i < a.length; i++)
-      {
-         if (a[i] == key) 
-         {
-           System.out.println(key+" is present at location "+(i+1));
-           break;
-         }
-      }
-      if (i == a.length)
-        System.out.println(key + " doesn't exist in array.");
-   }
+
+class Example1 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask user for the number of elements in the array
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        
+        // Initialize the array
+        int[] a = new int[n];
+
+        // Get array elements from user
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        // Ask user for the key to search
+        System.out.print("Enter the key to search for: ");
+        int key = scanner.nextInt();
+        
+        // Linear search
+        int i;
+        for (i = 0; i < a.length; i++) {
+            if (a[i] == key) {
+                System.out.println(key + " is present at location " + (i + 1));
+                break;
+            }
+        }
+
+        if (i == a.length) {
+            System.out.println(key + " doesn't exist in the array.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
 }
