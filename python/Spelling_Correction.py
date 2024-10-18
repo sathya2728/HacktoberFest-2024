1dd23318e1501917eecf79ceb362bf4cdc653398
sathya2728 <161938442+sathya2728@users.noreply.github.com>
2024-10-18 18:24:31 +0530

    Update Spelling_Correction.py
    code modified

--- python/Spelling_Correction.py
+++ python/Spelling_Correction.py
@@ -1,16 +1,18 @@
 from textblob import TextBlob
 
-def Convert(string):
-    li = list(string.split())
-    return li
-str1 = input("Enter your word :")
-words=Convert(str1)
+def convert_to_list(string):
+    """Convert a string into a list of words."""
+    return list(string.split())
+
+# Get input from the user
+input_string = input("Enter your sentence: ")
+words = convert_to_list(input_string)
 corrected_words = []
 
-for i in words:
-    corrected_words.append(TextBlob(i))
-print("wrong words : ", words)
-print("corrected words are : ")
-for i in corrected_words:
-    print(i.correct(), end=" ")
+# Correct each word
+for word in words:
+    corrected_words.append(str(TextBlob(word).correct()))
 
+# Display the results
+print("Original words: ", words)
+print("Corrected words: ", corrected_words)
