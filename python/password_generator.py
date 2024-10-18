f2bff322489382d06bfb37c9b6f5cc1ff2e2e887
sathya2728 <161938442+sathya2728@users.noreply.github.com>
2024-10-18 19:11:28 +0530

    Update password_generator.py
    code modified

--- python/password_generator.py
+++ python/password_generator.py
@@ -1,25 +1,31 @@
 import random
-def password_generator(pw_len):
+
+def password_generator(pw_lengths):
     data_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@$^&**_"
-    passwords_list = [] 
-    for i in pw_len:
-        password = '' 
-        for j in range(i):
+    passwords_list = []
+    
+    for length in pw_lengths:
+        password = ''
+        for _ in range(length):
             nxt_ind = random.randrange(len(data_string))
-            password = password + data_string[nxt_ind]
-        passwords_list.append(password) 
+            password += data_string[nxt_ind]  # Concatenate characters
+        passwords_list.append(password)
+    
     return passwords_list
- 
-n = int(input("Number of passwords to generate:"))
-print('Generating',n,'PASSWORDS')
-passwordLengths = []
+
+n = int(input("Number of passwords to generate: "))
+print('Generating', n, 'PASSWORDS')
+password_lengths = []
+
 for i in range(n):
-    print('NOTE==>Minimum length of password is 6!')
-    length = int(input('Enter the length of password' + str(i+1) + ':'))
-    if length<6:
+    print('NOTE: Minimum length of password is 6!')
+    length = int(input('Enter the length of password ' + str(i + 1) + ': '))
+    if length < 6:
         length = 6
-        print(' ')
-    passwordLengths.append(length)
-Password = password_generator(passwordLengths)
+        print('Length set to 6 due to minimum requirement.')
+    password_lengths.append(length)
+
+passwords = password_generator(password_lengths)
+
 for i in range(n):
-    print('Password',i+1,'=',Password[i])
+    print('Password', i + 1, '=', passwords[i])
